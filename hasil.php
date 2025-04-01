aca325835267c08f6869cc6d4d0fad2ced35e0bb
YujiroCokro <yujirocokro568@gmail.com>
2025-04-01 21:24:52 +0700

    Update hasil.php

--- hasil.php
+++ hasil.php
@@ -1,22 +1,36 @@
+<?php
+$text_align = $_POST['text-align'] ?? 'left';  // Default left jika tidak ada parameter
+$font_family = $_POST['font-family'] ?? 'Arial';  // Default Arial jika tidak ada parameter
+$color = $_POST['color'] ?? '#000000';  // Default black jika tidak ada parameter
+
+// Menyusun CSS berdasarkan pengaturan yang diterima
+$css = "text-align: $text_align; font-family: $font_family; color: $color;";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Hasil</title>
+    <style>
+        p{
+            <?= $css?>
+        }
+    </style>
 </head>
 <body>
-    <p>Halaman Hasil</p>
+    <h2>Halaman Hasil</h2>
     <br>
     <br>
     <br>
-    <p>Setting yang tersedia:</p>
-    <textarea rows="4" cols="50"></textarea>
+    <h2>Setting yang tersedia:</h2>
+    <textarea rows="4" cols="50"><?= $css?></textarea>
     <br>
     <a href="setting.php" >Ganti Setting</a>
     <br>
     <br>
-    <p>Contoh Paragraf</p>
+    <h3>Contoh Paragraf</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non repellendus earum reprehenderit laudantium! Omnis, possimus est. Officia maiores minus suscipit unde, provident nobis aut delectus molestiae eos, neque, reiciendis officiis!</p>
     
 
