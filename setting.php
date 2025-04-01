1f6253ca2d602e878fd2458376525ab6b0554e77
LouisVoterra <sunrisegamingiphone@gmail.com>
2025-04-01 20:08:22 +0700

    setting uda

--- setting.php
+++ setting.php
@@ -12,9 +12,14 @@
             setcookie("text-align",$_POST["text-align"],time() + 3600);
             setcookie("font-family",$_POST["font-family"],time() + 3600);
             setcookie("color",$_POST["color"],time() + 3600);
-            header("index.php");
+            header("Location: index.php");
             exit();
         }
+        //membaca cookie, kemudian disimpan ke variabel masing"
+        $text_align = isset($_COOKIE["text-align"]) ? $_COOKIE["text-align"] : "";
+        $font_family = isset($_COOKIE["font-family"]) ? $_COOKIE["font-family"] : "";
+        $color = isset($_COOKIE["color"]) ? $_COOKIE["color"] : "#000000"; // Default warna hitam jika tidak ada cookie
+
     ?>
 
     <p>Halaman Setting</p>
@@ -25,21 +30,26 @@
         <label>Text-align: </label> 
         <select name="text-align" id="text-align">  
             <option>-- Pilih Text Align --</option>
-            <option value="left">Left</option>
-            <option value="right">Right</option>
-            <option value="center">Center</option>
-            <option value="justify">Justify</option>
+            <option value="Left" <?= $text_align == 'Left' ? 'selected' : '' ?>>Left</option>
+            <option value="Right" <?= $text_align == 'Right' ? 'selected' : '' ?>>Right</option>
+            <option value="Center" <?= $text_align == 'Center' ? 'selected' : '' ?>>Center</option>
+            <option value="Justify" <?= $text_align == 'Justify' ? 'selected' : '' ?>>Justify</option>
+
         </select>
+        <br>
+        <br>
         <label>Font-Family: </label> 
         <select name="font-family">
             <option>-- Pilih Font Family --</option>
-            <option value="arial">Arial</option>
-            <option value="tahoma">Tahoma</option>
-            <option value="calibri">Calibri</option>
+            <option value="Arial" <?= $font_family == 'Arial' ? 'selected' : '' ?>>Arial</option>
+            <option value="Tahoma" <?= $font_family == 'Tahoma' ? 'selected' : '' ?>>Tahoma</option>
+            <option value="Calibri" <?= $font_family == 'Calibri' ? 'selected' : '' ?>>Calibri</option>
         </select>   
+        <br>
+        <br>
 
         <label>Color : </label> 
-        <input type="color" name="color" id="color" value="<?php $color ?>">
+        <input type="color" name="color" id="color" value="<?= $color ?>">
         <br>
         <br>
         <button type="submit">Simpan</button>
